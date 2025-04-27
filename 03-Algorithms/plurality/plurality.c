0b0bd22b0412614542d9fa2d7595800b3133d722
marinelegal <m.legall75@gmail.com>
2025-04-27 12:03:52 +0200

    plurality exercise done

--- 03-Algorithms/plurality/plurality.c
+++ 03-Algorithms/plurality/plurality.c
@@ -21,6 +21,7 @@ int candidate_count;
 // Function prototypes
 bool vote(string name);
 void print_winner(void);
+void bubble_sort(candidate candidates[]);
 
 int main(int argc, string argv[])
 {
@@ -52,7 +53,7 @@ int main(int argc, string argv[])
         string name = get_string("Vote: ");
 
         // Check for invalid vote
-        if (!vote(name))
+        if (!vote(name) && strcmp(name, candidates[i].name) != 0)
         {
             printf("Invalid vote.\n");
         }
@@ -65,13 +66,64 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+        if (strcmp(name, candidates[i].name) == 0)
+        {
+            // printf("Nb of votes before: %i\n", candidates[i].votes);
+            candidates[i].votes += 1;
+            // printf("Nb of votes after: %i\n", candidates[i].votes);
+            return true;
+        }
     return false;
 }
 
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    bubble_sort(candidates);
+
+    if (candidates[0].votes > candidates[1].votes)
+    {
+        printf("%s\n", candidates[0].name);
+        // printf("Winner is %s with %i votes\n", candidates[0].name, candidates[0].votes);
+    }
+    else
+    {
+        for (int i = 0; i < candidate_count; i++)
+        {
+            if (candidates[i].votes == candidates[0].votes)
+            {
+                printf("%s\n", candidates[i].name);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
     return;
+}
+
+void bubble_sort(candidate candidates[])
+{
+    int switched = 0;
+    for (int i = 0; i < (candidate_count - 1); i++)
+    {
+      // Bubble sort by descending order
+      for (int i = 0; i < (candidate_count - 1); i++)
+          {
+            if (candidates[i].votes < candidates[i+1].votes)
+            {
+                candidate tmp_candidate = candidates[i];
+                candidates[i] = candidates[i+1];
+                candidates[i+1] = tmp_candidate;
+                switched += 1;
+                // printf("Candidates %s and %s switched\n", candidates[i+1].name, candidates[i].name);
+            }
+          }
+      if (switched == 0)
+      {
+            break;
+      }
+      }
 }
